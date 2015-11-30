c13f33ca6839ca49e6758f6ad3bbc3f936699e41
Hiroki Sakamoto <taisho6339>
2015-12-01 07:02:11 +0900

    add app files

---
+++ app/src/main/scala/com/taishonet/droid/scala/MainActivity.scala
@@ -0,0 +1,11 @@
+package com.taishonet.droid.scala
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity extends AppCompatActivity {
+  override def onCreate(savedInstanceState: Bundle): Unit = {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
